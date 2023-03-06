65e668721b2cdf82ce93ce17c68487f1204289e0
divan <hlinakcurbag@gmail.com>
2023-03-06 11:06:20 +0300

    dfe

--- tgBot/bot.py
+++ tgBot/bot.py
@@ -8,21 +8,23 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.utils import executor
 
 
-# import gensim
+import gensim
 
-# w2v_fpath = "all.norm-sz100-w10-cb0-it1-min100.w2v"
-# w2v = gensim.models.KeyedVectors.load_word2vec_format(w2v_fpath, binary=True, unicode_errors='ignore')
-# w2v.init_sims(replace=True)
+w2v_fpath = "all.norm-sz100-w10-cb0-it1-min100.w2v"
+w2v = gensim.models.KeyedVectors.load_word2vec_format(w2v_fpath, binary=True, unicode_errors='ignore')
+w2v.init_sims(replace=True)
 
 
 bot = Bot(token=cf.TOKEN)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 
+
 class UserState(StatesGroup):
     name = State()
     address = State()
-
+    djghjc = State()
+  
 
 @dp.message_handler(commands=['reg'])
 async def user_register(message: types.Message):
@@ -34,16 +36,27 @@ async def user_register(message: types.Message):
 async def get_username(message: types.Message, state: FSMContext):
     await state.update_data(username=message.text)
     await message.answer("Отлично! Теперь введите ваш адрес.")
-    await UserState.next() # либо же UserState.adress.set()
-
+    await UserState.next() 
 
 @dp.message_handler(state=UserState.address)
 async def get_address(message: types.Message, state: FSMContext):
     await state.update_data(address=message.text)
-    data = await state.get_data()
-    await message.answer(f"Имя: {data['username']}\n"
-                         f"Адрес: {data['address']}")
+    await message.answer("Введите желаемый запрос")
+    await UserState.next() 
 
+
+
+@dp.message_handler(state=UserState.djghjc)
+async def get_djghjc(message: types.Message, state: FSMContext):
+    await state.update_data(djghjc=message.text)
+    data = await state.get_data()
+    lst = []
+    for word, score in w2v.most_similar(positive=[f"{data['djghjc']}"]):
+        lst.append(str(word)+' '+str(score)+'\n')
+    await message.answer(f"Имя: {data['username']}\n"
+                            f"Адрес: {data['address']}\n"
+                            f"Ответ на запрос: \n{''.join(lst)}")
+    lst.clear()
     await state.finish()
 
 if __name__ == "__main__":
--- tgBot/test.py
+++ tgBot/test.py
@@ -1,8 +1,18 @@
-import gensim
+# import gensim
 
-w2v_fpath = "all.norm-sz100-w10-cb0-it1-min100.w2v"
-w2v = gensim.models.KeyedVectors.load_word2vec_format(w2v_fpath, binary=True, unicode_errors='ignore')
-w2v.init_sims(replace=True)
+# w2v_fpath = "all.norm-sz100-w10-cb0-it1-min100.w2v"
+# w2v = gensim.models.KeyedVectors.load_word2vec_format(w2v_fpath, binary=True, unicode_errors='ignore')
+# w2v.init_sims(replace=True)
 
-for word, score in w2v.most_similar(positive=[u"птица", u"животное"], negative=[u"рыба"]):
-    print(word, score)
+# for word, score in w2v.most_similar(positive=[u"птица", u"животное"], negative=[u"рыба"]):
+#     print(word, score)
+
+lst = [1, 2, 3, 4]
+lst1 = [2, 4, 6, 8]
+
+l = []
+
+for i in range(len(lst)):
+    l.append({lst1[i]:lst[i]})
+
+print(str(l))
