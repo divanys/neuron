0b2baa93ca01b6907ed9e58351ab1064db288dd4
divan <hlinakcurbag@gmail.com>
2023-03-06 11:16:59 +0300

    ffffffff

--- newneuron/second.py
+++ newneuron/second.py
@@ -121,7 +121,8 @@ import matplotlib.pyplot as plt
 plt.plot(loss_arr)
 plt.show()
 
+
 probs = predict(accuracy)
 pred_class = np.argmax(probs)
 class_names = ['Setosa', 'Versicolor', 'Virginica']
-print('Predicted class:', class_names[pred_class])
+print('Predicted class:', class_names[pred_class]) 
